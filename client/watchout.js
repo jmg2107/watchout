c54be0e8c95bd998f41187e25cbb5ecbb4c28b16
jmg2107 <jenjengoo@gmail.com>
2016-06-17 15:25:20 -0700

    completed making the SVG enemy models

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,37 @@
 // start slingin' some d3 here.
+var width = 800;
+var height = 600;
+
+//draw the svg element
+var svg = d3.select(".board").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+//create random positions within width and height
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+//draw the enemies inside of the svg element
+
+//creates all the enemies to bind to the DOM via data
+var enemies = _.range(30);
+
+//binding the enemies to the actual svg elements and appending ellipses
+svg.selectAll("ellipse")
+  .data(enemies)
+  .enter()
+  .append("ellipse")
+  .attr("cx", function(){ return getRandomInt(width)})
+  .attr("cy", function(){ return getRandomInt(height)})
+  .attr("rx", "10")
+  .attr("ry", "10");
+
+
+
+
+
+
+
+
+
